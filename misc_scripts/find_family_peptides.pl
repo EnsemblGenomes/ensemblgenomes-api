6d28c60a12516ef166babac790691697b5c7106f
ds18 <>
2013-04-08 13:30:38 +0000

    updated comments etc.

--- misc_scripts/find_family_peptides.pl
+++ misc_scripts/find_family_peptides.pl
@@ -20,7 +20,7 @@
 
 =head1 DESCRIPTION
 
-This script is an example of how to use the Ensembl Bacteria compara database to find which genes are in a particular family
+This script is an example of how to use the Ensembl Bacteria compara database to find which genes are in a particular family and dump their peptides
 
 =head1 AUTHOR
 
@@ -46,19 +46,24 @@ my $helper = Bio::EnsEMBL::LookUp->new(-URL      => "http://bacteria.ensembl.org
 
 # load compara adaptor
 my $compara_dba = Bio::EnsEMBL::Compara::DBSQL::DBAdaptor->new(-HOST => 'mysql.ebi.ac.uk', -USER => 'anonymous', -PORT => '4157', -DBNAME => 'ensembl_compara_bacteria_17_70');
+
 # find the corresponding member
-my $family = $compara_dba->get_FamilyAdaptor()->fetch_by_stable_id('MF_00395');
-my $outfile= ">" . $family->stable_id . ".fa";
+my $family  = $compara_dba->get_FamilyAdaptor()->fetch_by_stable_id('MF_00395');
+
+# create a file to write to
+my $outfile = ">" . $family->stable_id . ".fa";
 my $seq_out = Bio::SeqIO->new(-file   => $outfile,
 							  -format => "fasta",);
 print "Writing family " . $family->stable_id() . " to $outfile\n";
+
+# loop over members
 for my $member (@{$family->get_all_Members()}) {
   my $genome_db = $member->genome_db();
   my ($member_dba) = @{$helper->get_by_name_exact($genome_db->name())};
   if (defined $member_dba) {
 	my $gene = $member_dba->get_GeneAdaptor()->fetch_by_stable_id($member->stable_id());
-	print "Writing sequence for ".$member->stable_id()."\n";
-	my $s    = $gene->canonical_transcript()->translate();
+	print "Writing sequence for " . $member->stable_id() . "\n";
+	my $s = $gene->canonical_transcript()->translate();
 	$seq_out->write_seq($s);
   }
 }
